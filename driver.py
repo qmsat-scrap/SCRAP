050bd869ca1f477a8b305dbe15bbd9b8c39a265b
kaizera-dev <kaizera-dev@outlook.com>
2026-04-27 00:45:45 +0100

    fps counter + imwrite()

--- driver.py
+++ driver.py
@@ -1,4 +1,7 @@
 import cv2
+import time
+import math
+from datetime import datetime
 
 working_cameras = []
 for i in range(0, 3):
@@ -6,7 +9,7 @@ for i in range(0, 3):
     success, frame = test_feed.read()
 
     if success:
-        print(f"Camera index{i} is functional.")
+        print(f"Camera index {i} is functional.")
         working_cameras.append(i)
     else:
         print(f"ERROR: Camera index {i} is not functional.")
@@ -22,11 +25,26 @@ else:
 camera_feeds = [cv2.VideoCapture(i) for i in working_cameras]
 no_of_cameras = len(camera_feeds)
 while True:
+    start = time.time()
 
     for i in range(0, no_of_cameras):
         flag, frame = camera_feeds[i].read()
         if flag:
+            end = time.time()
+            timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S-%f")[:-3]
+            filename = f"cam{i}_{timestamp}.jpg" #have separate folder for each cam?
+
+            cv2.imwrite(filename, frame)
+            cv2.putText(frame, 
+                        "FPS: " + str(math.ceil(1 / (end - start))),
+                        (10, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        1,
+                        (0, 0, 255),
+                        2,
+                        cv2.LINE_AA)    # FPS counter, just for observation         
             cv2.imshow('Feed ' + str(i+1), frame)
+
 
     if cv2.waitKey(20) == ord('q'):
         for i in range(0, no_of_cameras):
